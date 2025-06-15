d46e2bdd8e3616099be409a9e28ad99c76a78ef9
vbpendashev21 <117943303+vbpendashev21@users.noreply.github.com>
2025-06-15 22:49:21 +0300

    Add files via upload

---
+++ app/models.py
@@ -0,0 +1,67 @@
+from app import db
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from itsdangerous import URLSafeTimedSerializer
+from flask import current_app
+from datetime import datetime
+
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+
+class Prediction(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))  # optional
+    temperature = db.Column(db.Float, nullable=False)
+    condition = db.Column(db.String(20), nullable=False)
+    description = db.Column(db.String(255))
+    predicted_outfit = db.Column(db.String(100), nullable=False)
+    confidence = db.Column(db.Float)
+    feedback = db.Column(db.String(10))  # 'yes', 'no', or None
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    role = db.Column(db.String(20), default='user')
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+    def get_confirmation_token(self):
+        s = URLSafeTimedSerializer(current_app.config['SECRET_KEY'])
+        return s.dumps(self.email, salt='email-confirm')
+
+    @staticmethod
+    def verify_confirmation_token(token, expiration=3600):
+        s = URLSafeTimedSerializer(current_app.config['SECRET_KEY'])
+        try:
+            email = s.loads(token, salt='email-confirm', max_age=expiration)
+        except Exception:
+            return None
+        return email
+
+class Survey(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    temperature = db.Column(db.Float, nullable=False)
+    condition = db.Column(db.String(64), nullable=False)
+    outfit = db.Column(db.String(256), nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
+    user = db.relationship('User', backref=db.backref('surveys', lazy='dynamic'))
+
+class Feedback(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    prediction_id = db.Column(db.Integer, db.ForeignKey('prediction.id'))
+    rating = db.Column(db.Integer)  # 1–5
+    comment = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
